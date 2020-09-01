8f7689ae66d401c5b4a9377ad7d4bde9f0605ab9
JaniceCanedo <canedo@auburn.edu>
2020-09-01 17:41:57 -0400

    yes

---
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+	// @GetMapping("/")
+	// public String index() {
+	// 	return "Greetings from Spring Boot!";
+    // }
+    
+    @GetMapping("/")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
